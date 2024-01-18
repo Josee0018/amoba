5c7038c77147d277bec69bbf8ab569d0abca62ee
joseenrike <jalvarez0018@gmail.com>
2024-01-17 21:10:39 -0500

    test commit

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -19,7 +19,7 @@ const NavBar = (props) => {
               className="h-6 w-6 stroke-current"
               width="24"
               height="24"
-              viewBox="0 0 24 24"
+              viewBox="0 0 24 23"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
             >
